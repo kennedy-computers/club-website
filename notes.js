e0861c9fe240adfc9c7a22c3adb014061be71225
kennedy-computers <csclubkennedy@gmail.com>
2015-09-04 16:29:43 -0700

    changed

--- notes.js
+++ notes.js
@@ -4,16 +4,10 @@ $(function(){
 	var ratio = window.innerWidth/window.innerHeight;
 	var invRatio = 1/ratio;
 	var names = [
-		"Variables and Strings",
-		"Conditionals",
-		"Loops",
-		"Functions"
+		"Variables and Strings"
 	];//names of the notes
 	var urls = [
-		"https://docs.google.com/document/d/1EecgKsusFAbvUtwgjJZXX8lhX7MpzNZc-UVgYtCv6iw/edit",
-		"https://docs.google.com/document/d/1BFH8bY-CwMxBf3tHrMjN_i6xcTFTJG5IEnS4q5X6vQA/edit",
-		"https://docs.google.com/document/d/1dfM1GmaavLKGyvanC4gaPd9Y7ysU4Oe1Jj1jRwu6xm0/edit",
-		"https://docs.google.com/document/d/1_pabhdNj321AxN-n9liwuqZYmOJ2l6fLBS-6SEOb9os/edit"
+		"https://docs.google.com/document/d/1EecgKsusFAbvUtwgjJZXX8lhX7MpzNZc-UVgYtCv6iw/edit"
 	];//notes will be on google docs.
 	$.each(names, function(i, val){
 		//create a "box" for each notes topic
@@ -26,5 +20,5 @@ $(function(){
 	$(".notesbox").dblclick(function(){
 		location.href=urls[parseInt(this.id.replace("notes",""))];
 	}).height(invRatio*0.3*window.innerWidth);
-	
+
 });
